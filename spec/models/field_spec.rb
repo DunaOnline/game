8044cc5e66ed4477da9746e281298abb40ab405d
Doktor <doktoros@gmail.com>
2013-01-01 23:49:42 +0100

    test check_availability
    nejede :-/

--- spec/models/field_spec.rb
+++ spec/models/field_spec.rb
@@ -1,15 +1,35 @@
+# == Schema Information
+#
+# Table name: fields
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  planet_id  :integer          not null
+#  name       :string(255)      not null
+#  pos_x      :decimal(, )      default(0.0)
+#  pos_y      :decimal(, )      default(0.0)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
 
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Field do
-  before(:each) do
-    @field = Field.new
-  end
 
-  it "should desc" do
-    # TODO
+  it "check availabilty of popuation" do
+    user = create(:user)
+    field = create(:field, user: user)
+    puts field.to_yaml
+    puts field.resource.to_yaml
+
+#    check = field.check_availability('Population', 50000.0)
+#    expect(check).to eq(true)
+
+#    check = field.check_availability('Population', 150000.0)
+#    expect(check).to eq(false)
   end
 end
 
