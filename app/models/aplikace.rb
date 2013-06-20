4f2de3ebe90e2b17bbae800f4b43d0bbb2135d74
Doktor <doktoros@gmail.com>
2013-06-20 19:22:19 +0200

    hotfix - Wipe syselaady

--- app/models/aplikace.rb
+++ app/models/aplikace.rb
@@ -64,6 +64,7 @@ class Aplikace
     Influence.delete_all
     Law.delete_all
     Poll.delete_all
+    Syselaad.delete_all
     Post.delete_all
     Topic.delete_all
     Operation.delete_all
@@ -160,6 +161,37 @@ class Aplikace
     harvester = Building.where(:name => "Továrna na koření").first
     arrakis_field.postav(harvester, 10)
     puts "Budovy na Arrakis postaveny"
+
+    for house in House.playable do
+      Syselaad.create(
+          :house_id => house.id,
+          :subhouse_id => nil,
+          :kind => 'N',
+          :name => "Syselaad #{house.name}",
+          :description => "Syselaad národu #{house.name}"
+      )
+    end
+
+    Syselaad.create(
+        :house_id => nil, :subhouse_id => nil, :kind => 'I', :name => "Imperiální Syselaad",
+        :description => "Imperiální Syselaad")
+    Syselaad.create(
+        :house_id => nil, :subhouse_id => nil, :kind => 'L', :name => "Landsraadský Syselaad",
+        :description => "Landsraadský Syselaad")
+    Syselaad.create(
+        :house_id => nil, :subhouse_id => nil, :kind => 'S', :name => "Systémový Syselaad",
+        :description => "Systémový Syselaad")
+    Syselaad.create(
+        :house_id => nil, :subhouse_id => nil, :kind => 'E', :name => "Mezinárodní Syselaad",
+        :description => "Mezinárodní Syselaad")
+
+    for syselaad in Syselaad.all do
+      syselaad.topics << Topic.create(:syselaad_id => syselaad.id, :user_id => 1, :name => 'Úvod',
+                                      :last_poster_id => 1, :last_post_at => Time.now, )
+    end
+    for topic in Topic.all do
+      topic.posts << Post.create(:topic_id => topic.id, :user_id => 1, :content => 'Vítejte!')
+    end
 
     User.find_by_username('Doktor').jmenuj_spravcem
 
