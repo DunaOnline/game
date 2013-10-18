237fb3ecce50836e94f172fc7fd13d8a455d2170
minohimself <minohimself@gmail.com>
2013-10-18 22:17:25 +0200

    little change

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -31,8 +31,8 @@ class Ability
 
     if user
       if user.admin?
-	      #can :access, :rails_admin   # grant access to rails_admin
-	      #can :dashboard              # grant access to the dashboard
+	      can :access, :rails_admin   # grant access to rails_admin
+	      can :dashboard              # grant access to the dashboard
         can :manage, :all
       else
 	      cannot :access, :rails_admin   # grant access to rails_admin
